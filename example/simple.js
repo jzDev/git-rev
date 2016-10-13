f7db37006d753915791c85079b71dba0bf1c2b69
Johnny Zarate <johnnyzarate@Johnnys-MacBook-Pro.local>
2016-10-13 13:54:10 -0700

    Updated the simple examples.

--- example/simple.js
+++ example/simple.js
@@ -1,4 +1,37 @@
+// TODO - Create unit tests instead
 var git = require('../')
+
+/** Sync **/
+console.log('-- Synchronous Tests --');
+
+console.log('short', git.short([], { sync: true }));
+// => aefdd94
+
+console.log('long', git.long([], { sync: true }));
+// => aefdd946ea65c88f8aa003e46474d57ed5b291d1
+
+console.log('branch', git.branch([], { sync: true }));
+// => master
+
+console.log('tag', git.tag([], { sync: true }));
+// => 0.1.0
+
+console.log('log', git.log([], { sync: true }));
+// [ [ 'aefdd946ea65c88f8aa003e46474d57ed5b291d1',
+//     'add description',
+//     '7 hours ago',
+//     'Thomas Blobaum' ],
+//   [ '1eb9a6c8633a5a47a47487f17b17ae545d0e26a8',
+//     'first',
+//     '7 hours ago',
+//     'Thomas Blobaum' ],
+//   [ '7f85b750b908d28bfeb13ad6dba47d9d604508f9',
+//     'first commit',
+//     '2 days ago',
+//     'Thomas Blobaum' ] ]
+
+/** Async **/
+console.log('-- Asynchronous Tests --');
 
 git.short(function (str) {
   console.log('short', str)
@@ -35,6 +68,7 @@ git.log(function (array) {
   //     '2 days ago',
   //     'Thomas Blobaum' ] ]
 })
+
 git.exactTag(function(str){
   console.log('exactTag',str)
 })
